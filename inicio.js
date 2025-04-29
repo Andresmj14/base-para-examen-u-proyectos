b92f3c88deab7fc0c4fb34c6214c11dfb3b8779c
Andresmj14 <andresmjs11@gmail.com>
2025-04-29 10:54:20 -0500

    A commit message

--- inicio.js
+++ inicio.js
@@ -1,1 +1,2 @@
-git 
+import {Nombre, Apellido, Edad } from "./js/script.js";
+
---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+export let Nombre = "Javier";
+export let Apellido = "Gonzalez";
+export let Edad = 25;
+export let Ciudad = "Madrid";
+export let Ocupacion = "Desarrollador Web";
